4b0c27147c9673e5eb723a56960c553b6a040b3c
RhodyArthur <rhodaarthur48@gmail.com>
2025-09-01 15:30:27 +0000

    feat: add carousel implementation

--- src/app/layouts/auth/auth.ts
+++ src/app/layouts/auth/auth.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, inject } from '@angular/core';
+import { carouselReviews } from 'public/carouselData';
 
 @Component({
   selector: 'app-auth',
@@ -8,6 +9,11 @@ import { AfterViewInit, Component, ElementRef, inject } from '@angular/core';
 })
 export class Auth implements AfterViewInit {
   private el = inject(ElementRef);
+  private cdr = inject(ChangeDetectorRef);
+  reviews = carouselReviews;
+  currentReview: number = 0;
+  cycles: number = 0;
+  private intervalId!: number;
 
   ngAfterViewInit() {
     if (window.innerWidth <= 767) {
@@ -15,6 +21,28 @@ export class Auth implements AfterViewInit {
         const main = this.el.nativeElement.querySelector('main');
         main.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
       }, 3000);
+    }
+
+    this.intervalId = window.setInterval(() => {
+      this.currentReview = (this.currentReview + 1) % this.reviews.length;
+      this.cdr.detectChanges();
+
+      if (this.currentReview === this.reviews.length - 1) {
+        this.cycles++;
+        if (this.cycles >= 1) {
+          clearInterval(this.intervalId);
+        }
+      }
+    }, 2000);
+  }
+
+  goToReview(index: number) {
+    this.currentReview = index;
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
   }
 }
