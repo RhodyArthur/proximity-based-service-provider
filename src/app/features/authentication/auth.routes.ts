1f9931fdb282dc04863f1cec14638743e95d095b
RhodyArthur <rhodaarthur48@gmail.com>
2025-09-03 09:36:03 +0000

    feat: update authentication routes to include new routes

--- src/app/features/authentication/auth.routes.ts
+++ src/app/features/authentication/auth.routes.ts
@@ -5,4 +5,9 @@ export const authRoutes: Routes = [
     path: 'role-selection',
     loadComponent: () => import('./pages/roles/roles').then((m) => m.Roles),
   },
+  {
+    path: 'provider-agreement',
+    loadComponent: () =>
+      import('./pages/provider-agreement/provider-agreement').then((m) => m.ProviderAgreement),
+  },
 ];
