a3ae145cae65a84319c76ba51daef9ffccd3be6f
RhodyArthur <rhodaarthur48@gmail.com>
2025-09-08 15:12:05 +0000

    refactor: exclude carousel auto play

--- src/app/layouts/auth/auth.ts
+++ src/app/layouts/auth/auth.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject } from '@angular/core';
 import { carouselReviews } from 'public/carouselData';
 
 @Component({
@@ -9,11 +9,8 @@ import { carouselReviews } from 'public/carouselData';
 })
 export class Auth implements AfterViewInit {
   private el = inject(ElementRef);
-  private cdr = inject(ChangeDetectorRef);
   reviews = carouselReviews;
   currentReview: number = 0;
-  cycles: number = 0;
-  private intervalId!: number;
 
   ngAfterViewInit() {
     if (window.innerWidth <= 767) {
@@ -22,27 +19,9 @@ export class Auth implements AfterViewInit {
         main.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
       }, 3000);
     }
-
-    this.intervalId = window.setInterval(() => {
-      this.currentReview = (this.currentReview + 1) % this.reviews.length;
-      this.cdr.detectChanges();
-
-      if (this.currentReview === this.reviews.length - 1) {
-        this.cycles++;
-        if (this.cycles >= 1) {
-          clearInterval(this.intervalId);
-        }
-      }
-    }, 2000);
   }
 
   goToReview(index: number) {
     this.currentReview = index;
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
   }
 }
